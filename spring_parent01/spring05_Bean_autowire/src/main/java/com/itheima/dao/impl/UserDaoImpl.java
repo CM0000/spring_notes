1abdcf78aaa4aed186d63cfb414b901c07a5c7df
wszpan <wszpan@126.com>
2020-03-07 10:58:59 +0800

    Bean的装配,Autowire装配方式

---
+++ spring_parent01/spring05_Bean_autowire/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+    public void test() {
+        System.out.println("执行UserDao.test()方法；");
+    }
+}
---
+++ spring_parent01/spring05_Bean_autowire/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.UserDao;
+import com.itheima.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    private UserDao userDao;
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void test() {
+        userDao.test();
+        System.out.println("执行UserService.test()方法；");
+    }
+}
